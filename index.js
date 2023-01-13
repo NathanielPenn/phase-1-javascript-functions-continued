4ae5d0280dc152fbfe8a45a4fb3242cb8fa6a8a6
NatePennebaker <npenne1@aol.com>
2023-01-12 22:48:12 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    console.log(`This Saturday, I want to ${activity}!`);
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+function mondayWork(commitment = 'go to the office'){
+    console.log(`This Monday, I will ${commitment}.`);
+    return (`This Monday, I will ${commitment}.`);
+}
+mondayWork()
+mondayWork('work from home')
+
+function wrapAdjective(adjective) {
+    const innie = function (description = "goober") {
+        console.log(`You are ${adjective}${description}${adjective}!  `);
+        return `You are ${adjective}${description}${adjective}!`
+      }
+      console.log(innie)
+      return innie;
+}
+
+wrapAdjective('adjective')
